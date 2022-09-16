c4124d23b66d9095198a4b21acdb69f8a7a0e27a
ZharikovaES <z.kate2000@gmail.com>
2022-09-16 15:36:41 +0300

    1.1: added comments, fonts and replaced if with switch

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const score = document.querySelector('#score');
 const player = document.querySelector('#player');
 const line = document.querySelector('.tictactoe-line');
 
-let playerNow = 'X';
-
+// все возможные выигрышные комбинации на игровой сетке
 const winnings = [
     [0, 1, 2],
     [3, 4, 5],
@@ -17,6 +16,9 @@ const winnings = [
     [2, 4, 6],
 ];
 
+let playerNow = 'X';
+
+// класс игрока
 class Player {
     moves = new Array();
     score = 0;
@@ -36,6 +38,8 @@ class Player {
             return b - a;
         });
     };
+
+    // проверка на наличие выигрышной комбинации
     checkField(){
         this.sortMoves();
         for (let i = 0; i < this.moves.length; i++) {
@@ -43,7 +47,6 @@ class Player {
                 for (let k = 0; k < this.moves.length && i !== k && j !== k; k++) {
                     for (let m = 0; m < winnings.length; m++) {
                         if (this.moves[i] === winnings[m][0] && this.moves[j] === winnings[m][1] && this.moves[k] === winnings[m][2]) {
-                            console.log('Победа!', this.moves[i], this.moves[j], this.moves[k]);
                             this.createLine(this.moves[i], this.moves[j], this.moves[k]);
                             this.score++;
                             score.innerText = `${x.score} : ${o.score}`;
@@ -56,67 +59,67 @@ class Player {
         }
         return false;
     };
+
+    // отображение линии, перечеркивающей выигрышную комбинацию крестиков или кружков
     createLine(...profit) {
-        if (profit[1] === 4) {
-            if (profit[0] === 0){
-                line.style.transform = 'rotate(45deg) translate(-50%, -50%)';
-                line.style.top = '100%';
-                line.style.left = '28%';
-                line.style.width = Math.sqrt(tictactoe.offsetWidth ** 2 + tictactoe.offsetHeight ** 2) + 'px';
+            if (profit[1] === 4) {
+                switch (profit[0]) {
+                    case 0:
+                        line.style.transform = 'rotate(45deg) translate(-50%, -50%)';
+                        line.style.top = '100%';
+                        line.style.left = '28%';
+                        line.style.width = Math.sqrt(tictactoe.offsetWidth ** 2 + tictactoe.offsetHeight ** 2) + 'px';        
+                        break;
+                    case 1:
+                        line.style.transform = 'rotate(90deg)';
+                        line.style.top = '48%';
+                        line.style.left = '0%';
+                        line.style.width = tictactoe.offsetHeight + 'px';        
+                        break;
+                    case 2:
+                        line.style.transform = 'rotate(-45deg) translate(-50%, -50%)';
+                        line.style.top = '0%';
+                        line.style.left = '30%';
+                        line.style.width = Math.sqrt(tictactoe.offsetWidth ** 2 + tictactoe.offsetHeight ** 2) + 'px';        
+                        break;
+                    case 3:
+                        line.style.transform = 'translate(0%, -50%)';
+                        line.style.top = '50%';
+                        line.style.left = '0%';
+                        line.style.width = tictactoe.offsetWidth + 'px';        
+                        break;
+                }
                 line.style.display = 'block';
             }
-            if (profit[0] === 1){
-                line.style.transform = 'rotate(90deg)';
-                line.style.top = '48%';
-                line.style.left = '0%';
-                line.style.width = tictactoe.offsetHeight + 'px';
+            else if (profit[1] % 2 === 1) {
+                switch (profit[1]) {
+                    case 1:
+                        line.style.transform = 'translate(0%, -50%)';
+                        line.style.top = 'calc(100% / 6)';
+                        line.style.left = '0%';
+                        line.style.width = tictactoe.offsetWidth + 'px';
+                        break;
+                    case 3:
+                        line.style.transform = 'translate(0%, -50%) rotate(90deg)';
+                        line.style.top = '50%';
+                        line.style.left = '-34%';
+                        line.style.width = tictactoe.offsetHeight + 'px';
+                        break;
+                    case 5:
+                        line.style.transform = 'translate(0%, -50%) rotate(90deg)';
+                        line.style.top = '50%';
+                        line.style.left = '34%';
+                        line.style.width = tictactoe.offsetHeight + 'px';
+                        break;
+                    case 7:
+                        line.style.transform = 'translate(0%, -50%)';
+                        line.style.top = 'calc(100% * 5 / 6)';
+                        line.style.left = '0%';
+                        line.style.width = tictactoe.offsetWidth + 'px';
+                        break;
+                }
                 line.style.display = 'block';
             }
-            if (profit[0] === 2){
-                line.style.transform = 'rotate(-45deg) translate(-50%, -50%)';
-                line.style.top = '0%';
-                line.style.left = '30%';
-                line.style.width = Math.sqrt(tictactoe.offsetWidth ** 2 + tictactoe.offsetHeight ** 2) + 'px';
-                line.style.display = 'block';
-            }
-            if (profit[0] === 3){
-                line.style.transform = 'translate(0%, -50%)';
-                line.style.top = '50%';
-                line.style.left = '0%';
-                line.style.width = tictactoe.offsetWidth + 'px';
-                line.style.display = 'block';
-            }
-        }
-        if (profit[1] % 2 === 1) {
-            if (profit[1] === 1){
-                line.style.transform = 'translate(0%, -50%)';
-                line.style.top = 'calc(100% / 6)';
-                line.style.left = '0%';
-                line.style.width = tictactoe.offsetWidth + 'px';
-                line.style.display = 'block';
-            }
-            if (profit[1] === 3){
-                line.style.transform = 'translate(0%, -50%) rotate(90deg)';
-                line.style.top = '50%';
-                line.style.left = '-34%';
-                line.style.width = tictactoe.offsetHeight + 'px';
-                line.style.display = 'block';
-            }
-            if (profit[1] === 5){
-                line.style.transform = 'translate(0%, -50%) rotate(90deg)';
-                line.style.top = '50%';
-                line.style.left = '34%';
-                line.style.width = tictactoe.offsetHeight + 'px';
-                line.style.display = 'block';
-            }
-            if (profit[1] === 7){
-                line.style.transform = 'translate(0%, -50%)';
-                line.style.top = 'calc(100% * 5 / 6)';
-                line.style.left = '0%';
-                line.style.width = tictactoe.offsetWidth + 'px';
-                line.style.display = 'block';
-            }
-        }
     };
 }
 
@@ -125,6 +128,8 @@ const o = new Player();
 
 tictactoeCell.forEach((el, index) => {
     el.style.height = el.offsetWidth + 'px';
+
+    // функция, вызывающаяся после выбора игроком ячейки
     el.addEventListener('click', function() {
         if (this.classList.length === 1 && this.classList.contains('tictactoe__cell')) {
             if (playerNow === 'X') {
@@ -144,6 +149,7 @@ tictactoeCell.forEach((el, index) => {
     });
 });
 
+// очистка поля после завершения игры
 function clearField() {
     setTimeout(() => {
         playerNow = 'X';
@@ -158,6 +164,7 @@ function clearField() {
     }, 1500);
 }
 
+// функция проверяет, может ли игра продолжаться: если нет, то игра начинается сначала
 function checkCountMoves(player) {
     if (!player.checkField() && x.moves.length + o.moves.length === 9) {
         clearField();
